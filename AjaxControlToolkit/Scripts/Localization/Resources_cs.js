2b54ed221d8d5d9e5b2aad07333e8c74e2bdcd75
darilek <martin.darilek@gmail.com>
2016-03-15 13:49:50 +0100

    CS-cz localization for AjaxFileUpload control

--- AjaxControlToolkit/Scripts/Localization/Resources_cs.js
+++ AjaxControlToolkit/Scripts/Localization/Resources_cs.js
@@ -1,7 +1,9 @@
 ﻿Sys.Extended.UI.Localization["cs"] = {
     "Calendar_Today": "Dnes: {0}",
+
     "Common_DateTime_InvalidFormat": "Nesprávný formát data",
     "Common_DateTime_InvalidTimeSpan": "\"{0}\" není správný formát časového úseku",
+
     "PasswordStrength_DefaultStrengthDescriptions": "Neexistuje;Velmi chatrné;Chatrné;Slabé;Téměř použitelné;Téměř akceptovatelné;Průměrné;Dobré;Silné;Vynikající;Nezlomitlné!",
     "PasswordStrength_GetHelpRequirements": "Získat nápovědu k požadavkům na heslo",    
     "PasswordStrength_RemainingCharacters": "{0} dalších znaků",
@@ -9,5 +11,30 @@
     "PasswordStrength_RemainingNumbers": "{0} dalších čísel",
     "PasswordStrength_RemainingSymbols": "{0} dalších symbolů",
     "PasswordStrength_Satisfied": "Nic víc nevyžadováno",
-    "PasswordStrength_StrengthPrompt": "Síla: "
+    "PasswordStrength_StrengthPrompt": "Síla: ",
+
+    "AjaxFileUpload_DropFiles": "Sem přetáhněte soubory",
+    "AjaxFileUpload_AllFilesUploaded": "Všechny soubory byly nahrány.",
+    "AjaxFileUpload_SelectFile": "Vybrat...",
+    "AjaxFileUpload_Remove": "Odstranit",
+    "AjaxFileUpload_Upload": "Nahrát",
+    "AjaxFileUpload_Cancel": "Storno",
+    "AjaxFileUpload_FileList": "Seznam souborů pro nahrání:",
+    "AjaxFileUpload_SelectFileToUpload": "Vyberte soubor(y) pro nahrání.",
+    "AjaxFileUpload_Pending": "vyčkává",
+    "AjaxFileUpload_FileInQueue": "{0} soubor(ů) ve frontě.",
+    "AjaxFileUpload_UploadingHtml5File": "Nahrávám soubor: {0} o velikosti {1} B.",
+    "AjaxFileUpload_UploadingInputFile": "Nahrávám soubor: {0}.",
+    "AjaxFileUpload_UploadError": "Nastala neočekávaná chyba při nahrávání souboru.",
+    "AjaxFileUpload_DefaultError": "Chyba při nahrávání souboru.",
+    "AjaxFileUpload_UploadedPercentage": "odesláno {0} %",
+    "AjaxFileUpload_Uploading": "Nahrávám",
+    "AjaxFileUpload_Uploaded": "Odesláno",
+    "AjaxFileUpload_Cancelling": "Stornuji...",
+    "AjaxFileUpload_CancellingUpload": "Storno nahrávání...",
+    "AjaxFileUpload_UploadCanceled": "Odeslání souboru stornováno.",
+    "AjaxFileUpload_Canceled": "stornováno",
+    "AjaxFileUpload_error": "chyba",
+    "AjaxFileUpload_MaxNumberOfFilesExceeded": "Překročen max. povolený počet souborů",
+    "AjaxFileUpload_WrongFileType": "Není možné přidat soubor \u0027{0}\u0027 do seznamu k odeslání. Soubor typu \u0027{1}\u0027 není povolen"
 };
